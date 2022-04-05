13961341d2a13ebfb35192e9ccca9c49ccf130b8
xadityahx <adityamhegde@icloud.com>
2022-04-05 20:19:38 +0530

    3

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,2 +1,23 @@
-let name = "Aditya";
-console.log(name)
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors);
+console.log(selectedColors.length);
+
+
+// function types
+// Performing a task
+function greet(name, lastName) {
+    console.log('Well Hello ' + name + ' ' + lastName + ':D');
+}
+greet('Aditya', 'Hegde')
+greet('Aditya') // default value for a variable is undefined
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+let number = square(2);
+console.log(number);
+
+
