7e8f57c1c9a5656ca70187caa10cd41e4d469105
Aditya Hegde <91192289+bwaklog@users.noreply.github.com>
2022-06-22 09:40:09 +0530

    Update share.py

--- share.py
+++ share.py
@@ -1,4 +1,20 @@
-if __name__ == "__main__":
+def question1():
     d = sys.stdin.read()
     sys.stdout.write(d)
     sys.stderr.write("No error")
+
+def question2():
+    if __name__ == "__main__":
+    d = sys.stdin.read()
+    sys.stdout.write(d)
+    a = 0
+
+    for i in str(d):
+        try:
+            if i == "\n":
+                break
+            a += int(i)
+        except:
+            sys.stderr.write("Addition Error")
+            break
+    print("Sum = ", a)
